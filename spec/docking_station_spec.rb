6a2dcf3149259c560f0103b85e70544987898f8e
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-04 19:08:14 +0000

    refactored release working bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,10 @@ require 'docking_station'
 describe DockingStation do
   it 'releases bike' do
     should respond_to(:release_bike)
-    expect(subject.release_bike).not_to eq(nil)
-    expect(subject.release_bike.working?).to eq(true)
+  end
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
