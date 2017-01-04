c718b35b78e462e38937df7a42522f0a7bda6b13
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-04 18:57:47 +0000

    making docking station get bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,7 @@ require 'docking_station'
 describe DockingStation do
   it 'releases bike' do
     should respond_to(:release_bike)
+    expect(subject.release_bike).not_to eq(nil)
+    expect(subject.release_bike.working?).to eq(true)
   end
 end
